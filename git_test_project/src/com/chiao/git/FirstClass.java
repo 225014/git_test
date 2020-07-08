5a0cd81c42dc865b157c7cb7f6c63f87ea0f2182
Aoaoao <2495525162@qq.com>
2020-07-08 12:23:11 +0800

    添加FirstClass

---
+++ git_test_project/src/com/chiao/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.chiao.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+        System.out.println("bbbbbbbbb");
+        System.out.println("ccccccccc");
+    }
+}
